0c8ba4b9852bf0290ba44c6bd03d9a8cca9c00c5
Lucas Pereira <lucaspereiradesouzat@gmail.com>
2021-01-25 14:22:33 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import { buildSchema } from "graphql";
+
+const schema = buildSchema(`
+  type Query {
+    hello: String
+    random: Float!
+    rollDice(numDice: Int!, numSides: Int): [Int]
+  }
+`);
+
+const root = {
+  hello: () => "Hello world!",
+  random: () => {
+    return Math.random();
+  },
+  rollDice: (args) => {
+    var output = [];
+    for (var i = 0; i < args.numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (args.numSides || 6)));
+    }
+    return output;
+  },
+};
+
+const app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+app.listen(4000, () => console.log("Now browse to localhost:4000/graphql"));
